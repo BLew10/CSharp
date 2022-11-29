6be4117512c70a98d0cc1796d650ba5a975ed899
BLew10 <Brandonlewis.10@gmail.com>
2022-11-29 14:37:24 -0800

    restructure

---
+++ Fundamentals/RideMaker/Program.cs
@@ -0,0 +1,25 @@
+﻿Car newCar = new Car("gas", 10);
+Horse newHorse = new Horse("hay", 3);
+Bicycle newBicycle = new Bicycle();
+
+List <Vehicle> allVehicles = new List<Vehicle>();
+List <INeedFuel> allINeedFuels = new List<INeedFuel>();
+
+allVehicles.Add(newCar);
+allVehicles.Add(newHorse);
+allVehicles.Add(newBicycle);
+
+foreach(Vehicle vehicle in allVehicles)
+{
+    vehicle.ShowInfo();
+    if(vehicle is INeedFuel)
+    {
+        
+        allINeedFuels.Add((INeedFuel)vehicle);
+    }
+}
+
+foreach(INeedFuel vehicle in allINeedFuels)
+{
+    vehicle.GiveFuel(10);
+}
