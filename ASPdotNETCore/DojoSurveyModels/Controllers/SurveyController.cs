929ffda2b0107fe0c6e8dd46a2c4e62343ce80fa
BLew10 <Brandonlewis.10@gmail.com>
2022-11-30 11:32:09 -0800

    dojo survey

---
+++ ASPdotNETCore/DojoSurveyModels/Controllers/SurveyController.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyModels.Models;
+
+namespace DojoSurveyModels.Controllers;
+
+public class SurveyController : Controller
+{
+    private readonly ILogger<SurveyController> _logger;
+
+    public SurveyController(ILogger<SurveyController> logger)
+    {
+        _logger = logger;
+    }
+
+[HttpGet("")]
+    public IActionResult Form()
+    {
+        return View();
+    }
+
+[HttpGet("/result")]
+    public IActionResult Result(User user)
+    {
+        // Dictionary<string, string> UserInfo = new Dictionary<string,string>{{"Name", Name}, {"Location", Location}, {"Favorite Language", FavLang}, {"Comment", Comment}};
+        Console.WriteLine(user);
+
+        return View(user);
+    }
+
+
+    [HttpPost("/user/create")]
+    public IActionResult Create(User user)
+    {
+
+        if(ModelState.IsValid)
+        {
+            return View("Result", user);
+        }
+        else
+        {
+            return View("Form");
+        }
+
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
